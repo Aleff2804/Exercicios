41f93bcbcadfadb6cd7f2db107fca22be2b2efca
Aleff2804 <aleff.fernandes2804@gmail.com>
2026-03-17 21:28:31 -0300

    Update aula2.js

--- aula2.js
+++ aula2.js
@@ -1,20 +1,45 @@
-const usuarios = [
-  { nome: "Ana", idade: 20, ativo: true, compras: [100, 50, 25] },
-  { nome: "Bruno", idade: 17, ativo: false, compras: [30, 20] },
-  { nome: "Carlos", idade: 32, ativo: true, compras: [200, 150, 50, 100] },
-  { nome: "Diana", idade: 25, ativo: true, compras: [] },
-  { nome: "Eduardo", idade: 15, ativo: false, compras: [10] }
+const usuarios = [ 
+{ nome: "Ana", idade: 20, ativo: true, compras: [100, 50, 25] },
+{ nome: "Bruno", idade: 17, ativo: false, compras: [30, 20] }, 
+{ nome: "Carlos", idade: 32, ativo: true, compras: [200, 150, 50, 100] }, 
+{ nome: "Diana", idade: 25, ativo: true, compras: [] }, 
+{ nome: "Eduardo", idade: 15, ativo: false, compras: [10] } 
 ];
 
-//Parte 1
-for (let n of usuarios) {
-     console.log(n.compras[]);
-    }
-}
+// Parte 1
+usuarios.forEach(n => {
+    let soma = 0;
 
-//Parte 2
-for (let n of usuarios) {
-    if(n.ativo === true){
-        console.log(n.nome);
+    n.compras.forEach(valor => {
+        soma += valor;
+    });
+
+    console.log(n.nome + ": " + soma);
+});
+
+// Parte 2
+usuarios.forEach(n => {
+    if (n.ativo) {
+        console.log("O usuário: " + n.nome + " está ativo");
     }
-}
+});
+
+// Parte 3
+usuarios.forEach(n => {
+    if (n.idade >= 18) {
+        console.log(n.nome + " é maior de idade");
+    }
+});
+
+// Parte 4
+usuarios.forEach(n => {
+    let maior = 0;
+
+    n.compras.forEach(valor => {
+        if (valor > maior) {
+            maior = valor;
+        }
+    });
+
+    console.log(n.nome + ": " + maior);
+});
